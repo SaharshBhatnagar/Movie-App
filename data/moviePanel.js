ffaadc63b67de892c7d9216a245c1c24f02884af
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2025-09-07 19:55:32 +0530

    updated movieCards data

---
+++ data/moviePanel.js
@@ -0,0 +1,202 @@
+const movieData = [
+    {
+        image: "Movie-Images/Posters/BRAVE.jpg",
+        rating: 7.1,
+        title: "Brave"
+    },
+    {
+        image: "Movie-Images/Posters/Mulan.jpg",
+        rating: 7.7,
+        title: "Mulan"
+    },
+    {
+        image: "Movie-Images/Posters/Pete's-Dragon.jpg",
+        rating: 6.7,
+        title: "Pete's Dragon"
+    },
+    {
+        image: "Movie-Images/Posters/Kung-Fu-Panda.jpg",
+        rating: 7.6,
+        title: "Kung Fu Panda"
+    },
+    {
+        image: "Movie-Images/Posters/Raya-And-The-Last-Dragon.jpg",
+        rating: 7.3,
+        title: "Raya And The Last Dragon"
+    },
+    {
+        image: "Movie-Images/Posters/Cinderella.jpg",
+        rating: 7.3,
+        title: "Cinderella"
+    },
+    {
+        image: "Movie-Images/Posters/Dragon-Keeper.jpg",
+        rating: 6.2,
+        title: "Dragon Keeper"
+    },
+    {
+        image: "Movie-Images/Posters/Rio-2.jpg",
+        rating: 6.3,
+        title: "Rio 2"
+    },
+    {
+        image: "Movie-Images/Posters/The-Lion-King.jpg",
+        rating: 8.5,
+        title: "The Lion King"
+    },
+    {
+        image: "Movie-Images/Posters/The-BFG.jpg",
+        rating: 6.3,
+        title: "The BFG"
+    },
+    {
+        image: "Movie-Images/Posters/Frozen.jpg",
+        rating: 7.4,
+        title: "Frozen"
+    },
+    {
+        image: "Movie-Images/Posters/Moana.jpg",
+        rating: 7.6,
+        title: "Moana"
+    },
+    {
+        image: "Movie-Images/Posters/Smurfs.jpg",
+        rating: 6,
+        title: "Smurfs"
+    },
+    {
+        image: "Movie-Images/Posters/Snow-White.jpg",
+        rating: 7.6,
+        title: "Snow White"
+    },
+    {
+        image: "Movie-Images/Posters/Flow.jpg",
+        rating: 7.8,
+        title: "Flow"
+    },
+    {
+        image: "Movie-Images/Posters/UP.jpg",
+        rating: 8.3,
+        title: "Up"
+    },
+    {
+        image: "Movie-Images/Posters/The-wild-Robot.jpg",
+        rating: null,
+        title: "The Wild Robot"
+    },
+    {
+        image: "Movie-Images/Posters/How-To-Train-Your-Dragon.jpg",
+        rating: 8.1,
+        title: "How To Train Your Dragon"
+    },
+    {
+        image: "Movie-Images/Posters/A-Turtle's-Tale.jpg",
+        rating: 6,
+        title: "A Turtle's Tale"
+    },
+    {
+        image: "Movie-Images/Posters/Finding-Nemo.jpg",
+        rating: 8.2,
+        title: "Finding Nemo"
+    },
+    {
+        image: "Movie-Images/Posters/Zootopia.jpg",
+        rating: 8,
+        title: "Zootopia"
+    },
+    {
+        image: "Movie-Images/Posters/Mune.jpg",
+        rating: 7.2,
+        title: "Mune"
+    },
+    {
+        image: "Movie-Images/Posters/Cars.jpg",
+        rating: 7.2,
+        title: "Cars"
+    },
+    {
+        image: "Movie-Images/Posters/Abominable.jpg",
+        rating: 7,
+        title: "Abominable"
+    },
+    {
+        image: "Movie-Images/Posters/Aladdin.jpg",
+        rating: 8,
+        title: "Aladdin"
+    },
+    {
+        image: "Movie-Images/Posters/Bolt.jpg",
+        rating: 6.8,
+        title: "Bolt"
+    },
+    {
+        image: "Movie-Images/Posters/Maleficent.jpg",
+        rating: 6.9,
+        title: "Maleficent"
+    },
+    {
+        image: "Movie-Images/Posters/Mavka-The-Forest-Song.jpg",
+        rating: 7,
+        title: "Mavka: The Forest Song"
+    },
+    {
+        image: "Movie-Images/Posters/Ratatouille.jpg",
+        rating: 8.1,
+        title: "Ratatouille"
+    },
+    {
+        image: "Movie-Images/Posters/Tangled.jpg",
+        rating: 7.7,
+        title: "Tangled"
+    },
+    {
+        image: "Movie-Images/Posters/The-Jungle-Book.jpg",
+        rating: 7.4,
+        title: "The Jungle Book"
+    },
+    {
+        image: "Movie-Images/Posters/A-Silent-Voice.jpg",
+        rating: 8.2,
+        title: "A Silent Voice"
+    },
+    {
+        image: "Movie-Images/Posters/Monsters,-INC..jpg",
+        rating: 8.1,
+        title: "Monsters, Inc."
+    },
+    {
+        image: "Movie-Images/Posters/Shrek.jpg",
+        rating: 7.9,
+        title: "Shrek"
+    },
+    {
+        image: "Movie-Images/Posters/The-Angry-Birds.jpg",
+        rating: 6.3,
+        title: "The Angry Birds"
+    },
+    {
+        image: "Movie-Images/Posters/Toy-Story.jpg",
+        rating: 8.3,
+        title: "Toy Story"
+    },
+    {
+        image: "Movie-Images/Posters/Peter-Pan.jpg",
+        rating: 7.3,
+        title: "Peter Pan"
+    },
+    {
+        image: "Movie-Images/Posters/Beauty-And-The-Beast.jpg",
+        rating: 8,
+        title: "Beauty And The Beast"
+    },
+    {
+        image: "Movie-Images/Posters/The-Chronicles-of-Narnia.jpg",
+        rating: 6.9,
+        title: "The Chronicles of Narnia"
+    },
+    {
+        image: "Movie-Images/Posters/A-Bugs-Life.jpg",
+        rating: 7.2,
+        title: "A Bug's Life"
+    }
+];
